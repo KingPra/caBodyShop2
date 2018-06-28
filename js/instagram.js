76a453aa439bd48af8de6ccc7bc2a446e8afca70
King Pra <kingi.pra@gmail.com>
2018-06-28 18:27:31 -0400

    wrap up for the day

--- js/instagram.js
+++ js/instagram.js
@@ -1,7 +1,10 @@
-console.log('connected');
-
+//updates copyright
 function newYear () {
   return (new Date()).getFullYear()
 };
-
 document.querySelector('.new-year').innerHTML = newYear();
+
+
+//google place id
+const placeID = 'ChIJ4aAqOnygVogRfCVVrpv2454';
+const api_key = 'AIzaSyCMuYb8zu0i-Wih-7C1jAxoTrjs39OHYFI';
