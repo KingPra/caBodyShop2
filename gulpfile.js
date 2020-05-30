2a75b36a7f2f1958b7f99f0648909737ea17a5ca
KingPra <kingi.pra@gmail.com>
2020-05-30 10:19:49 -0400

    upgrade to gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,136 +1,138 @@
-var gulp = require('gulp');
-var less = require('gulp-less');
-var browserSync = require('browser-sync').create();
-var header = require('gulp-header');
-var cleanCSS = require('gulp-clean-css');
-var rename = require("gulp-rename");
-var uglify = require('gulp-uglify');
-var pkg = require('./package.json');
-const htmlMin = require('gulp-htmlmin');
-const imageMin = require('gulp-imagemin');
+"use strict"
 
-// Set the banner content
-var banner = ['/*!\n',
-    ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-    ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-    ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
-    ' */\n',
-    ''
-].join('');
+// Load plugins
+const autoprefixer = require("autoprefixer");
+const browsersync = require("browser-sync").create();
+const cp = require("child_process");
+const cssnano = require("cssnano");
+const del = require("del");
+const eslint = require("gulp-eslint");
+const gulp = require("gulp");
+const imagemin = require("gulp-imagemin");
+const newer = require("gulp-newer");
+const plumber = require("gulp-plumber");
+const postcss = require("gulp-postcss");
+const rename = require("gulp-rename");
+const sass = require("gulp-sass");
+const webpack = require("webpack");
+const webpackconfig = require("./webpack.config.js");
+const webpackstream = require("webpack-stream");
 
+//Browersync
 
-//minify html
-gulp.task('html', () => {
-  gulp.src('index.html')
-  .pipe(htmlMin({
-    collapseWhitespace: true,
-    removeComments: true
-  }))
-  .pipe(gulp.dest('cabodyshopllc.com/'))
-  .pipe(browserSync.reload({
-    stream: true
-  }))
-});
-
-//compress and move image to public folder
-gulp.task('img', () => {
-  gulp.src('img/*')
-  .pipe(imageMin([
-    imageMin.gifsicle({interlaced: true}),
-    imageMin.jpegtran({progressive: true}),
-    imageMin.optipng({optimizationLevel: 5}),
-    imageMin.svgo({
-        plugins: [
-            {removeViewBox: true},
-            {cleanupIDs: false}
-        ]
-    })
-]))
-  .pipe(gulp.dest('cabodyshopllc.com/img'))
-  .pipe(browserSync.reload({
-    stream: true
-  }))
-});
-
-// Compile LESS files from /less into /css
-gulp.task('less', function() {
-    return gulp.src('less/creative.less')
-        .pipe(less())
-        .pipe(header(banner, { pkg: pkg }))
-        .pipe(gulp.dest('css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
-});
-
-// Minify compiled CSS
-gulp.task('minify-css', ['less'], function() {
-    return gulp.src('css/creative.css')
-        .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('cabodyshopllc.com/css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
-});
-
-// Minify JS
-gulp.task('minify-js', function() {
-    return gulp.src('js/*')
-        .pipe(uglify())
-        .pipe(header(banner, { pkg: pkg }))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('cabodyshopllc.com/js'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
-});
-
-// Copy vendor libraries from /node_modules into /vendor
-gulp.task('copy', function() {
-    gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('cabodyshopllc.com/vendor/bootstrap'))
-
-    gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('cabodyshopllc.com/vendor/jquery'))
-
-    gulp.src(['node_modules/magnific-popup/dist/*'])
-        .pipe(gulp.dest('cabodyshopllc.com/vendor/magnific-popup'))
-
-    gulp.src(['node_modules/scrollreveal/dist/*.js'])
-        .pipe(gulp.dest('cabodyshopllc.com/vendor/scrollreveal'))
-
-    gulp.src([
-            'node_modules/font-awesome/**',
-            '!node_modules/font-awesome/**/*.map',
-            '!node_modules/font-awesome/.npmignore',
-            '!node_modules/font-awesome/*.txt',
-            '!node_modules/font-awesome/*.md',
-            '!node_modules/font-awesome/*.json'
-        ])
-        .pipe(gulp.dest('cabodyshopllc.com/vendor/font-awesome'))
-})
-
-// Run everything
-gulp.task('default', ['html', 'less', 'minify-css', 'minify-js', 'copy']);
-
-// Configure the browserSync task
-gulp.task('browserSync', function() {
-    browserSync.init({
+function browserSync(done) {
+    browsersync.init({
         server: {
-            baseDir: 'cabodyshopllc.com'
+            basedir: "./_site/"
         },
-    })
-})
+        port: 3000
+    });
+    done();
+}
 
-// Dev task with browserSync
-gulp.task('dev', ['browserSync', 'html', 'less', 'minify-css', 'minify-js', 'img'], function() {
-    gulp.watch('index.html', ['html']);
-    gulp.watch('less/*.less', ['less']);
-    gulp.watch('css/*.css', ['minify-css']);
-    gulp.watch('js/*.js', ['minify-js']);
-    gulp.watch('img/*.*', ['img'])
-    // Reloads the browser whenever HTML or JS files change
-    gulp.watch('*.html', browserSync.reload);
-    gulp.watch('js/**/*.js', browserSync.reload);
-});
+//Browsersync Reload
+function browserSyncReload(done) {
+    browsersync.reload();
+    done();
+}
+
+//Clean assets
+function clean() {
+    return del(["./_site/assets/"]);
+}
+
+//Optimize Images
+function images() {
+    return gulp
+    .src("./assets/img/**/*")
+    .pipe(newer("./_site/assets/img"))
+    .pipe(
+        imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.jpegtran({progrssive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {
+                        removeViewBox: false,
+                        collapseGroups: true
+                    }
+                ]
+            })
+        ])
+    )
+    .pipe(gulp.dest("./_site/assets/img"));
+}
+
+//CSS task
+function css() {
+    return gulp
+    .src("./assets/scss/**/*.scss")
+    .pipe(plumber())
+    .pipe(sass({ outputStyle: "expanded"}))
+    .pipe(gulp.dest("./_site/assets/css/"))
+    .pipe(rename({ suffix: ".min"}))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(gulp.dest("./_site/assts/css"))
+    .pipe(browsersync.stream());
+}
+
+//Lint scripts
+function scriptLint() {
+    return gulp
+    .src(["./assets/js/**/*", "./gulpfile.js"])
+    .pipe(plumber())
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+}
+
+//Transpile, concatenate and minify scripts
+function scripts() {
+    return (
+        gulp
+        .src(["./assets/js/**/*"])
+        .pipe(plumber())
+        .pipe(webpackstream(webpackconfig, webpack))
+        //folder only, filename is specified in webpack config
+        .pipe(gulp.dest("./_site/assets/js/"))
+        .pipe(browsersync.stream())
+    );
+}
+
+//Jekyll
+function jekyll() {
+    return cp.spawn("bundle", ["exec", "jekyll", "build"], {stdio: "inherit"});
+}
+
+//Watch files 
+function watchFiles() {
+    gulp.watch("./assets/scss/**/*", css);
+    gulp.watch("./assets/js/**/*", gulp.series(scriptLint, scripts));
+    gulp.watch(
+        [
+            "./_includes/**/*",
+            "./_layouts?**/*",
+            "./_pages/**/*",
+            "./_posts/**/*",
+            "./_projects/**/*"
+        ],
+        gulp.series(jekyll, browserSyncReload)
+    );
+    gulp.watch("./assets/img/**/*", images);
+}
+
+//define complex tasks
+const js = gulp.series(scriptLint, scripts);
+const build = gulp.series(clean, gulp.parallel(css, images, jekyll, js));
+const watch = gulp.parallel(watchFiles, browserSync);
+
+//export tasks
+exports.images = images;
+exports.css = css;
+exports.js = js;
+exports.jekyll = jekyll;
+exports.clean = clean;
+exports.build = build;
+exports.watch = watch;
+exports.default = build;
